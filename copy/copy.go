c2db1c85158235ae6ed7059f3d0a67fdce3b6ad6
Shaozhen Ding <dsz0111@gmail.com>
2018-06-04 21:40:07 -0400

    add copy

--- copy/copy.go
+++ copy/copy.go
@@ -1,1 +1,19 @@
 package copy
+
+// type CopyTo func(functions.CFObject, functions.API) error
+//
+// func Copy(another functions.API, functions.CFObject) functions.Action {
+// 	return func(object functions.CFObject) error {
+// 		o, err := from(object)
+// 		if err != nil {
+// 			return err
+// 		}
+// 		return to(o, another)
+// 	}
+// }
+
+// var FromOrgQuota From = func(org functions.CFObject) (functions.CFObject, error) {
+// 	api = org.GetAPI()
+//   orgName = org.GetName()
+//   orgQuota = api.
+// }
